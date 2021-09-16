e9fcd883b09143aacf07f7ef81d37ad956e1770f
DREBIZHEK <bogdanmosenz@gmail.com>
2021-09-16 12:35:33 +0400

    no message

---
+++ Abstract Factory/AbstractFactory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    public interface ISpaceshipFactory
+    {
+        IBattleSpaceship CrateBattleSpaceship();
+        IPeacefulSpaceship CreatePeacefulSpaceship();
+    }
+    public interface IBattleSpaceship
+    {
+		void Shoot();
+    }
+    public interface IPeacefulSpaceship
+    {
+		void Explore();
+
+	}
+	public class FastSpaceshipFactory : ISpaceshipFactory
+	{
+		public IBattleSpaceship CrateBattleSpaceship()
+		{
+			return new FastBattleSpaceship();
+		}
+
+		public IPeacefulSpaceship CreatePeacefulSpaceship()
+		{
+			return new FastPeacefulSpaceship();
+		}
+	}
+	public class HevySpaceshipFactory : ISpaceshipFactory
+	{
+		public IBattleSpaceship CrateBattleSpaceship()
+		{
+			return new HevyBattleSpaceship();
+		}
+
+		public IPeacefulSpaceship CreatePeacefulSpaceship()
+		{
+			return new HevyPeacefulSpaceship();
+		}
+	}
+	public class HevyBattleSpaceship : IBattleSpaceship
+	{
+		public void Shoot()
+		{
+			Console.WriteLine("Spaceship shoots BOOM BOOM");
+		}
+	}
+	public class FastBattleSpaceship : IBattleSpaceship
+	{
+		public void Shoot()
+		{
+			Console.WriteLine("Spaceship shoots PEW PEW");
+		}
+	}
+	public class HevyPeacefulSpaceship : IPeacefulSpaceship
+	{
+		public void Explore()
+		{
+			Console.WriteLine("I'm slowly exploring");
+		}
+	}
+	public class FastPeacefulSpaceship : IPeacefulSpaceship
+	{
+		public void Explore()
+		{
+			Console.WriteLine("I'm quickly exploring");
+		}
+	}
+}
---
+++ Patterns/Program.cs
@@ -0,0 +1,29 @@
+﻿using Abstract_Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Patterns
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var FastFactory = new FastSpaceshipFactory();
+			var HevyFactory = new HevySpaceshipFactory();
+
+			var FastBattle = FastFactory.CrateBattleSpaceship();
+			var FastPeaceful = FastFactory.CreatePeacefulSpaceship();
+			var HevyBattle = HevyFactory.CrateBattleSpaceship();
+			var HevyPeaceful = HevyFactory.CreatePeacefulSpaceship();
+
+			FastBattle.Shoot();
+			FastPeaceful.Explore();
+			HevyBattle.Shoot();
+			HevyPeaceful.Explore();
+		}
+	}
+}
