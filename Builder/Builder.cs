c910648300d95a606c27912e067f4387517dec34
DREBIZHEK <bogdanmosenz@gmail.com>
2021-09-23 15:48:18 +0400

    no message

---
+++ Builder/Builder.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class Builder
+    {
+        public interface IBuilder
+        {
+            void BuildDefaultCar();
+
+            void BuildLeatherInterior();
+
+            void BuildSportsEquipment();
+        }
+
+        public class ConcreteBuilder : IBuilder
+        {
+            private Product _product = new Product();
+
+            public ConcreteBuilder()
+            {
+                this.Reset();
+            }
+
+            public void Reset()
+            {
+                this._product = new Product();
+            }
+
+            public void BuildDefaultCar()
+            {
+                this._product.Add("Simple car");
+            }
+
+            public void BuildLeatherInterior()
+            {
+                this._product.Add("Leather Interior");
+            }
+
+            public void BuildSportsEquipment()
+            {
+                this._product.Add("Sports Equipment");
+            }
+
+            public Product GetProduct()
+            {
+                Product result = this._product;
+
+                this.Reset();
+
+                return result;
+            }
+        }
+
+        public class Product
+        {
+            private List<object> _parts = new List<object>();
+
+            public void Add(string part)
+            {
+                this._parts.Add(part);
+            }
+
+            public string ListParts()
+            {
+                string str = string.Empty;
+
+                for (int i = 0; i < this._parts.Count; i++)
+                {
+                    str += this._parts[i] + ", ";
+                }
+
+                str = str.Remove(str.Length - 2);
+
+                return "Product parts: " + str + "\n";
+            }
+        }
+
+        public class Director
+        {
+            private IBuilder _builder;
+
+            public IBuilder Builder
+            {
+                set { _builder = value; }
+            }
+
+            public void BuildMinimalViableProduct()
+            {
+                this._builder.BuildDefaultCar();
+            }
+
+            public void BuildFullFeaturedProduct()
+            {
+                this._builder.BuildDefaultCar();
+                this._builder.BuildLeatherInterior();
+                this._builder.BuildSportsEquipment();
+            }
+        }
+    }
+}
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,16 +1,24 @@
 ﻿using Abstract_Factory;
+using Singleton;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static Builder.Builder;
 
 namespace Patterns
 {
 	class Program
 	{
 		static void Main(string[] args)
+		{
+			//Factory();
+			//Builder();
+			Singleton();
+		}
+
+		static void Factory()
 		{
 			var FastFactory = new FastSpaceshipFactory();
 			var HevyFactory = new HevySpaceshipFactory();
@@ -24,6 +32,41 @@ namespace Patterns
 			FastPeaceful.Explore();
 			HevyBattle.Shoot();
 			HevyPeaceful.Explore();
+		}
+
+		static void Builder()
+		{
+			var director = new Director();
+			var builder = new ConcreteBuilder();
+			director.Builder = builder;
+
+			Console.WriteLine("Basic configuration:");
+			director.BuildMinimalViableProduct();
+			Console.WriteLine(builder.GetProduct().ListParts());
+
+			Console.WriteLine("Full configuration:");
+			director.BuildFullFeaturedProduct();
+			Console.WriteLine(builder.GetProduct().ListParts());
+
+			Console.WriteLine("Custom configuration:");
+			builder.BuildDefaultCar();
+			builder.BuildSportsEquipment();
+			Console.Write(builder.GetProduct().ListParts());
+		}
+
+		static void Singleton()
+		{
+			Singleton.Singleton s1 = global::Singleton.Singleton.GetInstance();
+			Singleton.Singleton s2 = global::Singleton.Singleton.GetInstance();
+
+			if (s1 == s2)
+			{
+				Console.WriteLine("Singleton works, both variables contain the same instance.");
+			}
+			else
+			{
+				Console.WriteLine("Singleton failed, variables contain different instances.");
+			}
 		}
 	}
 }
