b77f5e7f9248661a73d65e04a79afdfe0dcdc3ec
DREBIZHEK <bogdanmosenz@gmail.com>
2021-11-18 11:16:38 +0400

    no message

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Flyweight;
 using static Builder.Builder;
 using static Strategy.Strategy;
 
@@ -17,7 +19,58 @@ namespace Patterns
 			//Factory();
 			//Builder();
 			//Singleton();
-			Strategy();
+			//Strategy();
+			//Flyweight();
+		}
+
+		static void Flyweight()
+        {
+			// Клиентский код обычно создает кучу предварительно заполненных
+			// легковесов на этапе инициализации приложения.
+			var factory = new FlyweightFactory(
+				new Car { Company = "Chevrolet", Model = "Camaro2018", Color = "pink" },
+				new Car { Company = "Mercedes Benz", Model = "C300", Color = "black" },
+				new Car { Company = "Mercedes Benz", Model = "C500", Color = "red" },
+				new Car { Company = "BMW", Model = "M5", Color = "red" },
+				new Car { Company = "BMW", Model = "X6", Color = "white" }
+			);
+			factory.listFlyweights();
+
+			addCarToPoliceDatabase(factory, new Car
+			{
+				Number = "CL234IR",
+				Owner = "James Doe",
+				Company = "BMW",
+				Model = "M5",
+				Color = "red"
+			});
+
+			addCarToPoliceDatabase(factory, new Car
+			{
+				Number = "CL234IR",
+				Owner = "James Doe",
+				Company = "BMW",
+				Model = "X1",
+				Color = "red"
+			});
+
+			factory.listFlyweights();
+
+			void addCarToPoliceDatabase(FlyweightFactory Factory, Car car)
+			{
+				Console.WriteLine("\nClient: Adding a car to database.");
+
+				var flyweight = Factory.GetFlyweight(new Car
+				{
+					Color = car.Color,
+					Model = car.Model,
+					Company = car.Company
+				});
+
+				// Клиентский код либо сохраняет, либо вычисляет внешнее состояние и
+				// передает его методам легковеса.
+				flyweight.Operation(car);
+			}
 		}
 
 		static void Factory()
